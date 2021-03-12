8109e977b08432b8e15029d989444d51bb6f6a29
caco-jr <jr.carlosad@gmail.com>
2021-03-11 21:06:08 -0300

    refac: Renomeando nome do projeto

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import Container from '@material-ui/core/Container';
 
 import * as S from './Header.styles';
+import { PROJECT_NAME } from '@utils/strings/index';
 
 const Header = (): JSX.Element => (
   <S.Wrapper>
     <Container fixed>
-      <h1>Header</h1>
+      <S.Title>{PROJECT_NAME}</S.Title>
     </Container>
   </S.Wrapper>
 );
