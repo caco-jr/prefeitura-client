e791e6a8588cd4e2309e6c5dbe2babe249f7c2ad
Carlos Costa <jr.carlosad@gmail.com>
2021-03-13 19:44:47 -0300

    test: Ajuste teste

--- src/views/MedicalAppointment/components/List/List.test.tsx
+++ src/views/MedicalAppointment/components/List/List.test.tsx
@@ -4,7 +4,7 @@ import MedicalAppointmentList from './List';
 
 describe('List', () => {
   it('should render the heading', () => {
-    const { getByRole } = render(<MedicalAppointmentList />);
+    const { getByRole } = render(<MedicalAppointmentList list={[]} />);
 
     expect(getByRole('heading', { name: /List/i })).toBeInTheDocument();
   });
