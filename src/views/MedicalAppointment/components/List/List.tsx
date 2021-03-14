9f95f412db00419deba749e06e7c4e4dcd8dfb50
Carlos Costa <jr.carlosad@gmail.com>
2021-03-14 15:22:54 -0300

    style: Estilizando listagem de consultas

--- src/views/MedicalAppointment/components/List/List.tsx
+++ src/views/MedicalAppointment/components/List/List.tsx
@@ -1,3 +1,4 @@
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -15,6 +16,7 @@ type IProps = {
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    width: '100%',
   },
   title: {
     fontSize: 14,
@@ -29,37 +31,41 @@ const MedicalAppointmentList = ({ list }: IProps): JSX.Element => {
 
   return (
     <S.Wrapper>
-      {list.map(({ type, hospital, time }, index) => (
-        <Card className={classes.root} variant="outlined" key={index}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color="textSecondary"
-              gutterBottom
-            >
-              {time}
-            </Typography>
+      <Grid container spacing={3}>
+        {list.map(({ type, hospital, time }, index) => (
+          <Grid container item xs={12} md={4} key={index}>
+            <Card className={classes.root} variant="outlined">
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {time}
+                </Typography>
 
-            <Typography variant="h5" component="h2">
-              {type}
-            </Typography>
+                <Typography variant="h5" component="h2">
+                  {type}
+                </Typography>
 
-            <Typography className={classes.pos} color="textSecondary">
-              <RiHospitalLine /> {hospital}
-            </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  <RiHospitalLine /> {hospital}
+                </Typography>
 
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
+                <Typography variant="body2" component="p">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
 
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-      ))}
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </S.Wrapper>
   );
 };
