55092d0565838d040ffbff4c59669da509ecdafa
Carlos Costa <jr.carlosad@gmail.com>
2021-03-14 12:57:27 -0300

    feat: Aplicando link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,22 @@
 import Container from '@material-ui/core/Container';
+import Link from 'next/link';
 
 import * as S from './Navbar.styles';
 
 const menuList = [
-  { label: 'Saúde' },
-  { label: 'Agropecuária' },
-  { label: 'Meio Ambiente' },
+  { label: 'Saúde', to: '/consultas-medicas' },
+  { label: 'Agropecuária', to: '/' },
+  { label: 'Meio Ambiente', to: '/' },
 ];
 
 const Navbar = (): JSX.Element => (
   <S.Wrapper>
     <Container fixed>
       <S.List>
-        {menuList.map(({ label }, index) => (
-          <S.ListItem key={`${label}-${index}`}>{label}</S.ListItem>
+        {menuList.map(({ label, to }, index) => (
+          <Link href={`${to}`} key={`${label}-${index}`}>
+            <S.ListItem href={`${to}`}>{label}</S.ListItem>
+          </Link>
         ))}
       </S.List>
     </Container>
