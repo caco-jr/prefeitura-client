8ad2572641aec971c537e67fa3524769306d966d
Carlos Costa <jr.carlosad@gmail.com>
2021-03-14 12:32:57 -0300

    style: Estilizando o Header

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: var(--tertiary-color);
+`;
 
-export const Title = styled.span``;
+export const Title = styled.span`
+  color: #fff;
+  font-weight: bold;
+  font-size: var(--title-md);
+`;
