703865e7c11342683973809074dead4ea33e7f31
Carlos Costa <jr.carlosad@gmail.com>
2021-03-14 16:13:14 -0300

    feat: Criando os campos de select

--- src/views/MedicalAppointment/components/Add/Add.tsx
+++ src/views/MedicalAppointment/components/Add/Add.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -12,13 +11,18 @@ import {
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 import { GrScheduleNew } from 'react-icons/gr';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 import * as S from './Add.styles';
 
 const MedicalAppointmentAdd = (): JSX.Element => {
   const [open, setOpen] = useState(false);
-
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [type, setType] = useState('');
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -30,6 +34,10 @@ const MedicalAppointmentAdd = (): JSX.Element => {
 
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
+  };
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setType(event.target.value as string);
   };
 
   return (
@@ -71,14 +79,39 @@ const MedicalAppointmentAdd = (): JSX.Element => {
             />
           </MuiPickersUtilsProvider>
 
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
+          <FormControl required>
+            <InputLabel id="demo-simple-select-required-label">Tipo</InputLabel>
+
+            <Select
+              labelId="demo-simple-select-required-label"
+              id="demo-simple-select-required"
+              value={type}
+              onChange={handleChange}
+            >
+              <MenuItem value="pediatra">Pediatra</MenuItem>
+              <MenuItem value="oftalmologista">Oftalmologista</MenuItem>
+            </Select>
+
+            <FormHelperText>Required</FormHelperText>
+          </FormControl>
+
+          <FormControl required>
+            <InputLabel id="demo-simple-select-required-label">
+              Hospital
+            </InputLabel>
+
+            <Select
+              labelId="demo-simple-select-required-label"
+              id="demo-simple-select-required"
+              value={type}
+              onChange={handleChange}
+            >
+              <MenuItem value="pediatra">Pediatra</MenuItem>
+              <MenuItem value="oftalmologista">Oftalmologista</MenuItem>
+            </Select>
+
+            <FormHelperText>Required</FormHelperText>
+          </FormControl>
         </DialogContent>
 
         <DialogActions>
