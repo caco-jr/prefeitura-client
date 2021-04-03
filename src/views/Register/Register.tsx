b72639ecf012e148fd0fb8859e9c295284767389
Carlos Costa <jr.carlosad@gmail.com>
2021-04-03 16:34:53 -0300

    feat: Criando Formulário para cadastro de usuário

--- src/views/Register/Register.tsx
+++ src/views/Register/Register.tsx
@@ -1,11 +1,14 @@
 import * as S from './Register.styles';
 import Layout from '@components/Layout';
+import RegisterForm from './components/Form';
 
 export const RegisterPage = (): JSX.Element => (
   <Layout title="Register">
     <section className="container">
       <S.Wrapper>
         <h1>Cadastro</h1>
+
+        <RegisterForm />
       </S.Wrapper>
     </section>
   </Layout>
---
+++ src/views/Register/components/Form/RegisterForm.tsx
@@ -0,0 +1,90 @@
+import { useForm, Controller } from 'react-hook-form';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import Button from '@material-ui/core/Button';
+
+import * as S from './RegisterForm.styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+type IProps = {};
+
+const RegisterForm = ({}: IProps): JSX.Element => {
+  const { handleSubmit, register, control } = useForm();
+
+  const classes = useStyles();
+
+  const onSubmit = data => console.log(data);
+
+  return (
+    <S.Wrapper className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+      <TextField label="Nome" inputRef={register} name="name" required />
+      <TextField
+        label="Sobrenome"
+        inputRef={register}
+        name="lastName"
+        required
+      />
+
+      <TextField
+        label="Nome completo da mãe"
+        inputRef={register}
+        name="motherName"
+        required
+      />
+
+      <TextField label="CPF" inputRef={register} name="cpf" required />
+      <TextField label="RG" inputRef={register} name="rg" required />
+      <TextField label="E-mail" inputRef={register} name="email" required />
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Controller
+          name="birthDate"
+          control={control}
+          required
+          defaultValue={new Date()}
+          render={({ onChange, value }) => (
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="dd/MM/yyyy"
+              margin="normal"
+              label="Data de nascimento"
+              value={value}
+              onChange={onChange}
+              maxDate={new Date()}
+              KeyboardButtonProps={{
+                'aria-label': 'Altere a data',
+              }}
+            />
+          )}
+        />
+      </MuiPickersUtilsProvider>
+
+      <TextField
+        label="Senha"
+        inputRef={register}
+        name="password"
+        type="password"
+        required
+      />
+
+      <Button type="submit">Cadastrar</Button>
+    </S.Wrapper>
+  );
+};
+
+export default RegisterForm;
