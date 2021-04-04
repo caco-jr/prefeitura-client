9946457e60497be71445e578a96c56fc7cd92137
Carlos Costa <jr.carlosad@gmail.com>
2021-04-04 11:48:21 -0300

    feat: Criando botão para a página de login

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,17 +1,36 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import Button from '@material-ui/core/Button';
+
 import * as S from './Login.styles';
 import Layout from '@components/Layout';
 import LoginForm from './components/Form';
 
-export const LoginPage = (): JSX.Element => (
-  <Layout title="Login">
-    <section className="container">
-      <S.Wrapper>
-        <S.Title>Login</S.Title>
+export const LoginPage = (): JSX.Element => {
+  const { query } = useRouter();
 
-        <LoginForm />
-      </S.Wrapper>
-    </section>
-  </Layout>
-);
+  return (
+    <Layout title="Login">
+      <section className="container">
+        <S.Wrapper>
+          <S.Title>Login</S.Title>
+
+          <LoginForm />
+
+          <Link href={{ pathname: '/cadastro', query }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              className="button-redirect"
+            >
+              Não tem conta? Crie uma aqui
+            </Button>
+          </Link>
+        </S.Wrapper>
+      </section>
+    </Layout>
+  );
+};
 
 export default LoginPage;
