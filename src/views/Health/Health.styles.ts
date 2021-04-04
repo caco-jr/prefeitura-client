892ba370fc9ddc3f1c50cf0d6e4238d9b430b39c
Carlos Costa <jr.carlosad@gmail.com>
2021-04-04 12:26:48 -0300

    feat: Inserindo opções na tela de saúde

--- src/views/Health/Health.styles.ts
+++ src/views/Health/Health.styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.section``;
+
+export const Title = styled.h1`
+  margin: 45px 0 60px;
+`;
+
+export const CardWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 30px;
+
+  .c-card {
+    cursor: pointer;
+
+    &__content {
+      text-align: center;
+      font-size: var(--text-lg);
+    }
+  }
+`;
--- src/views/Health/Health.tsx
+++ src/views/Health/Health.tsx
@@ -1,11 +1,33 @@
 import * as S from './Health.styles';
+import Link from 'next/link';
 import Layout from '@components/Layout';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 export const HealthPage = (): JSX.Element => (
   <Layout title="Health">
     <section className="container">
       <S.Wrapper>
-        <h1>Health</h1>
+        <S.Title>O que você gostaria de consultar?</S.Title>
+
+        <S.CardWrapper>
+          <Link href="/consultas-medicas">
+            <Card className="c-card">
+              <CardContent className="c-card__content">
+                Consultas médicas
+              </CardContent>
+            </Card>
+          </Link>
+
+          <Link href="/centros-de-saude">
+            <Card className="c-card">
+              <CardContent className="c-card__content">
+                Instituições de saúde
+              </CardContent>
+            </Card>
+          </Link>
+        </S.CardWrapper>
       </S.Wrapper>
     </section>
   </Layout>
