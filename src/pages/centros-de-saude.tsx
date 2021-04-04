fd13e419c07b3c869fee2de11faad5b652636b27
Carlos Costa <jr.carlosad@gmail.com>
2021-04-03 22:43:53 -0300

    feat: Preparando redirecionamento

--- src/pages/centros-de-saude.tsx
+++ src/pages/centros-de-saude.tsx
@@ -1,9 +1,9 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import HealthCenterService from '@services/api/healthCenter';
 
 export { default } from '@views/HealthCenters';
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   try {
     const healthCentersAPI = new HealthCenterService();
 
@@ -11,8 +11,13 @@ export const getStaticProps: GetStaticProps = async () => {
 
     return { props: { list: healthCenters } };
   } catch (err) {
-    console.error(err);
+    console.error(err.data);
 
-    return { props: { errors: '' } };
+    return {
+      redirect: {
+        destination: '/login?redirectTo="/centros-de-saude"',
+        permanent: false,
+      },
+    };
   }
 };
