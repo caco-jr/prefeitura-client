827e6ab5c38536ce93654ca17fe78a80bb6bf1a3
Carlos Costa <jr.carlosad@gmail.com>
2021-04-05 20:45:56 -0300

    Endpoint correta

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,3 @@
-export const API_EXTERNAL_URL = 'http://localhost:8080';
+export const API_EXTERNAL_URL = 'https://tcc-bom-destino-api.herokuapp.com';
 
 export const API_URL = 'http://localhost:3000/api';
