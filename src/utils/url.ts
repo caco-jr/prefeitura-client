f944e35cda3372fd2c095db27451caba4602a8db
Carlos Costa <jr.carlosad@gmail.com>
2021-04-10 13:35:32 -0300

    refac: Apontando para em prod

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,3 @@
 export const API_EXTERNAL_URL = 'https://tcc-bom-destino-api.herokuapp.com';
 
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://tcc-bom-destino-client.vercel.app/api';
