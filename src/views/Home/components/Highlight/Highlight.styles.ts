29e2df247a072aa781db58faad599b42021a66be
Carlos Costa <jr.carlosad@gmail.com>
2021-04-10 18:02:25 -0300

    style: Ajuste de imagem

--- src/views/Home/components/Highlight/Highlight.styles.ts
+++ src/views/Home/components/Highlight/Highlight.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   margin-top: 30px;
   display: grid;
-  grid-template-columns: repeat(2, minmax(320px, 1fr));
+  grid-template-columns: repeat(2, minmax(250px, 1fr));
   gap: 8px;
 
   @media (max-width: 500px) {
